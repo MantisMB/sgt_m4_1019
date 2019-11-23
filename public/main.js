1cef8173fae6b7d99d4cb3504b8253bedf7628be
mantismb <matt.d.baldwin@gmail.com>
2019-11-23 14:14:08 -0800

    Added grade details #1

---
+++ public/main.js
@@ -0,0 +1,59 @@
+$(document).ready(initApp);
+
+function initApp(){
+    console.log('App Initialized');
+
+    getStudents();
+    
+};
+
+function getStudents(){
+    const config = {
+        url: '/api/students',
+        success: resp => {
+            console.log('Response:', resp);
+            addStudentsToDom(resp.students);
+        }
+    }
+    
+    
+    $.ajax(config);
+};
+
+function addStudentsToDom(students){
+    const studentElements = [];
+    const tbody = $('#student-data');
+
+    students.forEach(student => {
+        const tr = $('<tr>');
+        const name = $('<td>', {text: student.name});
+        const course = $('<td>', {text: student.course});
+        const grade = $('<td>', {text: student.grade});
+        
+
+        tbody.text('');
+
+        tr.append(name, course, grade);
+
+        studentElements.push(tr);
+    });
+
+    tbody.append(studentElements);
+
+};
+
+function addStudent(name, course, grade){
+    $.ajax({
+        method: 'POST',
+        url: '/api/students',
+        data: {
+            name: name,
+            course: course,
+            grade: grade
+        },
+        success: (resp) => {
+            console.log('Student Added:', resp);
+            getStudents();
+        },
+    })
+}
